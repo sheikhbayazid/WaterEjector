9d772bee98dd0a6de1e58546ffabea2e0ba1666d
Sheikh Bayazid <sheikhbayazid@gmail.com>
2020-12-23 16:06:10 +0600

    Initial Commit

---
+++ WaterEjector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterEjector
+//
+//  Created by Sheikh Bayazid on 12/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaterEjector/WaterEjectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterEjectorApp.swift
+//  WaterEjector
+//
+//  Created by Sheikh Bayazid on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct WaterEjectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
